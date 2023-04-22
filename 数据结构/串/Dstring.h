c11afef4fb0b709d9b941094b25304a8f8cdfa55
雪樱 <2651955182@qq.com>
2023-04-22 20:46:46 +0800

    513

---
+++ 数据结构/串/Dstring.h
@@ -0,0 +1,70 @@
+typedef struct{
+    char *str;
+    int maxLength;
+    int length;
+}DString;
+
+void Initiate(DString *S,int max,char *string){
+    int i;
+    S->str=(char *)malloc(sizeof(char)*max);
+    S->maxLength=max;
+    S->length=strlen(string);
+    for(i=0;i<S->length;i++)
+    S->str[i]=string[i];
+}
+int Insert(DString *S,int pos,DString T){
+    int i;
+    if(pos < 0){
+        printf("参数pos出错！");
+        return 0;
+    }
+    else{
+        if(S->length+T.length>S->maxLength){
+            realloc(S->str,(S->length+T.length)*sizeof(char));
+            S->maxLength=S->length+T.length;
+        }
+    }       
+    for(i=S->length-1;i>=pos;i--)
+        S->str[i+T.length]=S->str[i];
+    for(i=0;i<T.length;i++)
+        S->str[pos+i]=T.str[i];
+    S->length+=T.length;
+    return 1;
+}
+int Delete(DString *S,int pos,int len){
+    int i;
+    if(S->length<=0){
+        printf("数组中未存放字符无元素可删！\n");
+        return 0;
+    }
+    else if(pos<0||len<0||pos+len>S->length){
+        printf("参数pos和len不合法");
+        return 0;
+    }
+    else{
+        for(i=pos+len;i<=S->length-1;i++)
+            S->str[i-len]=S->str[i];
+            S->length=S->length-len;
+            return 1;
+    }
+}
+int SubString(DString *S,int pos,int len,DString *T){
+    int i;
+    if(pos<0||len<0||pos+len>S->length){
+        printf("参数pos和len出错！");
+        return 0;
+    }
+    if(len>T->maxLength){
+        T->str=(char *)malloc(len*sizeof(char));
+        T->maxLength=len;
+    }
+    for(i=0;i<len;i++)
+        T->str[i]=S->str[pos+i];
+    T->length=len;
+    return 1;
+}
+void Destroy(DString *S){
+    free(S->str);
+    S->maxLength=0;
+    S->length;
+}
---
+++ 数据结构/串/main.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"Dstring.h"
+int main(){
+    DString myString1,myString2,myString3;
+    int i,max1=5,max2=9,max3=0;
+    Initiate(&myString1,max1,"Data");
+    Initiate(&myString2,max2,"Structure");
+    Initiate(&myString3,max3," ");
+    printf("初始化myString2串：        ");
+    for(i=0;i<myString2.length;i++)
+        printf("%c",myString2.str[i]);
+    printf("    maxLength = %d",myString2.maxLength);
+    printf("    length = %d\n",myString2.length);
+    Insert(&myString2,0,myString1);
+    printf("插入子串后myString2串：    ");
+    for(i=0;i<myString2.length;i++)
+        printf("%c",myString2.str[i]);
+    printf("    maxLength = %d",myString2.maxLength);
+    printf("    length = %d\n",myString2.length);
+    Delete(&myString2,0,5);
+    printf("删除子串后myString2串：    ");
+    for(i=0;i<myString2.length;i++)
+        printf("%c",myString2.str[i]);
+    printf("    maxLength = %d",myString2.maxLength);
+    printf("    length = %d\n",myString2.length);
+    SubString(&myString2,0,5,&myString3);
+    printf("取子串后myString3串：    ");
+    for(i=0;i<myString3.length;i++)
+        printf("%c",myString3.str[i]);
+    printf("    maxLength = %d",myString3.maxLength);
+    printf("    length = %d\n",myString3.length);
+    Destroy(&myString1);
+    Destroy(&myString2);
+    Destroy(&myString3);
+    return 0;
+}
