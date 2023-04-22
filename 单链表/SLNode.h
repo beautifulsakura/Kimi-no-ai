363b324ba5a521c42ecca8259d9554620785e99a
雪樱 <2651955182@qq.com>
2023-04-22 15:57:46 +0800

    数据结构

--- 单链表/SLNode.h
+++ 单链表/SLNode.h
@@ -40,5 +40,43 @@ int ListDelete(SLNode *head,int i,DataType *x){
     int j;
     p=head;
     j=-1;
-    while(p->next!=NULL)
+    while(p->next!=NULL&&p->next->next!=NULL&&j<i-1){
+        p=p->next;
+        j++;
+    }
+    if(j!=i-1){
+        printf("删除元素位置参数错误！");
+        return 0;
+    }
+    s=p->next;
+    *x=s->data;
+    p->next=p->next->next;
+    free(s);
+    return 1;
+}
+int ListGet(SLNode *head,int i,DataType *x){
+    SLNode *p;
+    int j;
+    p=head;
+    j=-1;
+    while(p->next !=NULL && j<i){
+        p=p->next;
+        j++;
+    }
+    if(j!=i){
+        printf("取元素未知参数错误！");
+        return 0;
+    }
+    *x=p->data;
+    return 1;
+}
+void Destroy(SLNode **head){
+    SLNode *p,*p1;
+    p=*head;
+    while(p!=NULL){
+        p1=p;
+        p=p->next;
+        free(p1);
+    }
+    *head=NULL;
 }
---
+++ 单链表/main.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef int DataType;
+#include"SLNode.h"
+
+int main(){
+    SLNode *head;
+    int i,x;
+    ListInitiate(&head);
+    for(i=0;i<10;i++)
+        ListInsert(head,i,i+1);
+    ListDelete(head,4,&x);
+    for(i=0;i<ListLength(head);i++){
+        ListGet(head,i,&x);
+        printf("%d  ",x);
+    }
+    Destroy(&head);
+}
